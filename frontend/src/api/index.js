5b1d795afb89da18650e5381b251c67fc5246d81
jawad63969-driod <jawad.63969@iqra.edu.pk>
2026-04-19 19:56:28 +0500

    Updated Live link

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://supermarket-mvp.onrender.com/api';
 
 export const searchProducts = async (query) => {
     try {
